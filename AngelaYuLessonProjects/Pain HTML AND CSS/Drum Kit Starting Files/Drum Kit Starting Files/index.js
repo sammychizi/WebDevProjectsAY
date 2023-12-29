2f9ae9bb0f500006885033f4f81a2292783787fd
Samantha <chizalemasammy@gmail.com>
2023-12-29 21:23:38 +0200

    added sounds to buttons. now when a button is clicked, a acorresponding sound is made

--- AngelaYuLessonProjects/Pain HTML AND CSS/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ AngelaYuLessonProjects/Pain HTML AND CSS/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,16 +1,51 @@
-document.querySelector("button").addEventListener("click", playSound);
+
 var numberDrums = document.querySelectorAll(".drum").length;
 
-for(i=0; i < numberDrums; i++){
+for(var i=0; i < numberDrums; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", 
         function(){
-            this.style.color = "white";
+            
+            var buttonClicked = this.innerHTML;
+
+            switch(buttonClicked){
+                case "w":
+                    var audio = new Audio("sounds/tom-4.mp3");
+                    audio.play();
+                break; 
+                case "a":
+                    var audio = new Audio("sounds/crash.mp3");
+                    audio.play();
+                break;
+                case "s":
+                    var audio =  new Audio("sounds/tom-1.mp3");
+                    audio.play();
+                break;
+                case "d":
+                    var audio = new Audio("sounds/tom-2.mp3");
+                    audio.play();
+                break;
+                case "j":
+                    var audio = new Audio("sounds/tom-3.mp3");
+                    audio.play();
+                break;
+                case "k":
+                    var audio = new Audio("sounds/kick-bass.mp3");
+                    audio.play();
+                break;
+                case "l":
+                    var audio = new Audio("sounds/snare.mp3");
+                    audio.play();
+                break;
+                default:
+                    console.log(buttonClicked)
+                break;
+            }
         }
     )
 }
 
 
-function playSound(){
-    var sound = new Audio('sounds/tom-1.mp3');
-    sound.play();
-}
+// function playSound(){
+//     var sound = new Audio('sounds/tom-1.mp3');
+//     sound.play();
+// }
