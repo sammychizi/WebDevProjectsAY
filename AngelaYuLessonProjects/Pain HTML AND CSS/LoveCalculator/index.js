476c30beb3841c803859da00c74f69bbe3c3807e
Samantha <chizalemasammy@gmail.com>
2024-01-02 17:27:15 +0200

    sounds are added

--- AngelaYuLessonProjects/Pain HTML AND CSS/LoveCalculator/index.js
+++ AngelaYuLessonProjects/Pain HTML AND CSS/LoveCalculator/index.js
@@ -1,9 +1,15 @@
+
+
 document.querySelector("#btn").addEventListener("click", function(){
     var num = Math.random() * 100;
-    var lovePercent = (Math.floor(num +1));
+var lovePercent = (Math.floor(num +1));
 
-    var name1 = document.querySelector("#name1").value;
-    var name2 = document.querySelector("#name2").value;
-
-    document.querySelector("h1").innerHTML = name1 + " and " + name2 + " love each other " + lovePercent + "%"
+var name1 = document.querySelector("#name1").value;
+var name2 = document.querySelector("#name2").value;
+    document.querySelector("h1").innerHTML = name1 + " and " + name2 + " love each other " + lovePercent + "%";
+    showLovePercentText()
 })
+
+function showLovePercentText(){
+    return document.ad "You love each other a lot!"
+}
